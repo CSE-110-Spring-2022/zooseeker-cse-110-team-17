a6e0507e0201d768a426b6111bdbb17d9672fff8
EvanLiu0 <ealiu@ucsd.edu>
2022-05-01 10:22:06 -0700

    Updated MainActivity to prepare for merge

--- app/src/main/java/com/example/team17zooseeker/MainActivity.java
+++ app/src/main/java/com/example/team17zooseeker/MainActivity.java
@@ -30,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         Context context = this;
-
         ZooKeeperDatabase database = ZooKeeperDatabase.getSingleton(context);
 
         nodeDao = database.nodeItemDao();
@@ -39,51 +38,16 @@ public class MainActivity extends AppCompatActivity {
         List<nodeItem> nodes = nodeDao.getAll();
         List<edgeItem> edges = edgeDao.getAll();
 
-//        for(int i = 0; i < nodes.size(); i++)
-//            Log.e("Node:", nodes.get(i).toString());
-//
-//        for(int i = 0; i < edges.size(); i++)
-//            Log.e("Edge:", edges.get(i).toString());
-
         Map<String, nodeItem> nodeMap = nodes.stream().collect(Collectors.toMap(nodeItem::getId, Function.identity()));
         Map<String, edgeItem> edgeMap = edges.stream().collect(Collectors.toMap(edgeItem::getId, Function.identity()));
 
-        Map<String, nodeItem> vInfo = null;
-        Map<String, edgeItem> eInfo = null;
-
-        try {
-            vInfo = nodeItem.loadNodeInfoJSON(context, "node.json");
-            eInfo = edgeItem.loadEdgeInfoJSON(context, "edge.json");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-//        vInfo.forEach((key, value) -> Log.d("Our Node", key + ":" + value));
-//        nodeMap.forEach((key, value) -> Log.d("Their Node", key + ":" + value));
-
-        Graph<String, IdentifiedWeightedEdge> g = null;
-        try {
-            g = graphItem.loadZooGraphJSON(context,"graph.json");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         Button plan = findViewById(R.id.plan_btn);
         EditText searchBar = findViewById(R.id.search_text);
-
 
         plan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String query = (searchBar.getText()).toString();
-
-                nodeItem queryResult = nodeMap.get(query);
-
-                if(queryResult != null)
-                    plan.setText("Found Exhibit " + query);
-                else
-                    plan.setText("Could Not Find Exhibit " + query);
 
             }
         });
