9f7c08f7b5c67bab35785ac19c273d16693cfd84
EvanLiu0 <ealiu@ucsd.edu>
2022-05-08 09:05:28 -0700

    Added second integration test

--- app/src/androidTest/java/com/example/team17zooseeker/AddExhibitTests.java
+++ app/src/androidTest/java/com/example/team17zooseeker/AddExhibitTests.java
@@ -14,87 +14,130 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.internal.util.Checks.checkNotNull;
 import static org.hamcrest.Matchers.allOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
 
+import android.content.Context;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.Lifecycle;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
+import androidx.test.annotation.UiThreadTest;
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.matcher.BoundedMatcher;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+import androidx.test.rule.UiThreadTestRule;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.w3c.dom.Node;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @LargeTest
 @RunWith( AndroidJUnit4.class )
 public class AddExhibitTests {
+    ZooKeeperDatabase testDb;
+    NodeItemDao nodeItemDao;
+
+    @Before
+    public void resetDatabase() {
+        Context context = ApplicationProvider.getApplicationContext();
+        testDb = Room.inMemoryDatabaseBuilder(context, ZooKeeperDatabase.class)
+                .allowMainThreadQueries()
+                .build();
+        ZooKeeperDatabase.injectTestDatabase(testDb);
+    }
 
     @Test
-    public void exampleActivityTest() {
+    public void testAddValidExhibit() {
         ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
-
+//        scenario.moveToState(Lifecycle.State.CREATED);
+//        scenario.moveToState(Lifecycle.State.STARTED);
+//        scenario.moveToState(Lifecycle.State.RESUMED);
         scenario.onActivity(activity -> {
-            // GIVEN (this test database state)
-            ZooKeeperDatabase testDatabase = Room.inMemoryDatabaseBuilder(activity, ZooKeeperDatabase.class)
-                    .allowMainThreadQueries()
-                    .build();
-
-            // load json for testing
-            // insert into test db
             List<String> temp = new ArrayList<String>();
-            testDatabase.nodeItemDao().insert(new nodeItem("lions", "exhibit", "Lions", temp));
-            testDatabase.nodeItemDao().insert(new nodeItem("gorillas", "exhibit", "Gorillas", temp));
+            testDb.nodeItemDao().insert(new nodeItem("lions", "exhibit", "Lions", temp));
+            testDb.nodeItemDao().insert(new nodeItem("gorillas", "exhibit", "Gorillas", temp));
+            testDb.nodeItemDao().getAll();
+            nodeItem temp1 = testDb.nodeItemDao().get("lions");
+            nodeItem temp2 = testDb.nodeItemDao().get("gorillas");
+            Log.e("Node 1: ", temp1.toString());
+            Log.e("Node 2: ", temp2.toString());
 
-            ZooKeeperDatabase.injectTestDatabase(testDatabase);
+            EditText searchText = activity.findViewById(R.id.search_text);
+            RecyclerView vh = activity.findViewById(R.id.visitation_list_view);
+            NodeListAdapter adapter = new NodeListAdapter();
+            adapter.setHasStableIds(true);
+            vh.setAdapter(adapter);
 
-            // WHEN
-            ViewInteraction textEntry = onView(allOf(withId(R.id.search_text), isDisplayed()));
-            textEntry.perform(replaceText("Lions"), closeSoftKeyboard());
-            textEntry.perform(pressImeActionButton());
+            searchText.requestFocus();
+            searchText.setText("Lions");
+            searchText.clearFocus();
+            List<nodeItem> addedNodesList = new ArrayList<nodeItem>();
 
-            //ViewInteraction planButton = onView(allOf(withId(R.id.plan_btn), isDisplayed()));
-            //planButton.perform(click());
-            //ViewInteraction visitationView = onView(allOf(withId(R.id.visitation_list_view), isDisplayed()));
+            Log.e("Node 1: ", temp1.name);
+            Log.e("Text Search: ", String.valueOf(searchText.getText()));
+            if(String.valueOf(searchText.getText()).equals(temp1.name)) {
+                Log.e("ENTERED HERE!", "HELLO");
+                addedNodesList.add(temp1);
+                adapter.setNodeItems(addedNodesList);
+            }
 
-            // THEN
-            // assert by checking that the item is indeed in the plans view
-            // onView -> getText -> assert, etc
-            String insertedName = "Lions";
-            //onView(withText(insertedName)).check(matches(isDisplayed()));
-            onView(withId(R.id.visitation_list_view))
-                    .check(matches(atPosition(0, withText(insertedName))));
+            Log.e("Search Text: ", String.valueOf(searchText.getText()));
+            Log.e("Adapter: ->>>", String.valueOf(adapter.getItemCount()));
+            Log.e("VH: ", String.valueOf(adapter.getItemName(0)));
+            assertEquals(String.valueOf(searchText.getText()), String.valueOf(adapter.getItemName(0)));
+
         });
     }
-    public static Matcher<View> atPosition(final int position, @NonNull final Matcher<View> itemMatcher) {
-        checkNotNull(itemMatcher);
-        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("has item at position " + position + ": ");
-                itemMatcher.describeTo(description);
-            }
 
-            @Override
-            protected boolean matchesSafely(final RecyclerView view) {
-                RecyclerView.ViewHolder viewHolder = view.findViewHolderForAdapterPosition(position);
-                if (viewHolder == null) {
-                    // has no item on such position
-                    return false;
-                }
-                return itemMatcher.matches(viewHolder.itemView);
-            }
-        };
+    @Test
+    public void testAddInvalidExhibit() {
+        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
+//        scenario.moveToState(Lifecycle.State.CREATED);
+//        scenario.moveToState(Lifecycle.State.STARTED);
+//        scenario.moveToState(Lifecycle.State.RESUMED);
+        scenario.onActivity(activity -> {
+            List<String> temp = new ArrayList<String>();
+            testDb.nodeItemDao().insert(new nodeItem("lions", "exhibit", "Lions", temp));
+            testDb.nodeItemDao().insert(new nodeItem("gorillas", "exhibit", "Gorillas", temp));
+            testDb.nodeItemDao().getAll();
+
+            nodeItem temp1 = testDb.nodeItemDao().get("Tapirs");
+
+            EditText searchText = activity.findViewById(R.id.search_text);
+            RecyclerView vh = activity.findViewById(R.id.visitation_list_view);
+            NodeListAdapter adapter = new NodeListAdapter();
+            adapter.setHasStableIds(true);
+            vh.setAdapter(adapter);
+
+            searchText.requestFocus();
+            searchText.setText("Tapirs");
+            searchText.clearFocus();
+            List<nodeItem> addedNodesList = new ArrayList<nodeItem>();
+
+            assertNull(temp1);
+
+        });
     }
 
 }
-
